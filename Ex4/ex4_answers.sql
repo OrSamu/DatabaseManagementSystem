d63d3653071d9a3f6772c0a8c7cf0cec8ff92986
Or Samucian <orfacemailj@gmail.com>
2022-04-13 18:41:15 +0300

    quastion 1

--- Ex4/ex4_answers.sql
+++ Ex4/ex4_answers.sql
@@ -1,22 +1,9 @@
--- israel, israeli, 1234569
+-- Or, Samuican, 315872424
 
---question a
-select emp_id, emp_fname, emp_lname, count(*) 
-from sales_order S, employee E 
-where S.sales_rep = E.emp_id
-group by emp_id;
-
--- question b
-select * 
-from product
-where name like '%Shirt%';
-
---- question c
-select count(*), count(distinct name) 
-from product
-where name like '%Shirt%';
-
---- question d
-select emp_id, emp_fname, emp_lname
-from employee, department
-where dept_head_id == emp_id;
+--question 1: Find all customers that has ID thats bigger than all the ID's from customers in Taxes
+SELECT *
+FROM customer 
+WHERE state != 'TX'
+AND id > (SELECT MAX(id)
+FROM customer 
+WHERE state == 'TX');
