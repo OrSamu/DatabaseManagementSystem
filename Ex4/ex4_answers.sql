6ab599ba656f6c7b6b535d24c9201f531a1fbbb3
Or Samucian <orfacemailj@gmail.com>
2022-04-24 08:51:44 +0300

    final

--- Ex4/ex4_answers.sql
+++ Ex4/ex4_answers.sql
@@ -14,10 +14,9 @@ FROM customer
 WHERE address LIKE '%x%';
 
 --quastion 3: Find all customers (id, fname, lname) that all of their orders are from 1994
-// all the clients minus clients with orders from 1993
 SELECT C.id, C.fname, C.lname, S.id, S.order_date
 FROM customer C, saLes_order S
-WHERE C.id = S.cust_id;
+WHERE C.id = S.cust_id
 EXCEPT
 SELECT C.id, C.fname, C.lname, S.id, S.order_date
 FROM customer C, saLes_order S
@@ -82,59 +81,62 @@ SELECT MAX(order_date)
 FROM sales_order;
 
 --quastion 11: Which state has the most customers
+SELECT state, MAX(C.customers_num)
+FROM (SELECT state, count(id) as customers_num
+FROM customer
+GROUP BY state) C;
 
+--quastion 12: Find all customers (id, first and last name) who have at least one sale order that worker 129 was assigned to
+SELECT C.id, C.fname, C.lname
+FROM customer C, (SELECT cust_id
+FROM sales_order
+WHERE sales_rep = 129
+GROUP BY cust_id) S
+WHERE C.id = S.cust_id;
 
+--quastion 13: Create a view that will have sale order and order cost
+--quastion 13 A: Create the view
+CREATE VIEW orders_cost
+AS SELECT S.id AS order_id, SUM(P.unit_price) AS order_cost
+FROM product P, sales_order_items S
+WHERE P.id = S.prod_id
+GROUP BY S.id;
+--quastion 13 B: Calculate the costs of all the orders of customer 101
+SELECT S.cust_id, SUM(C.order_cost)
+FROM sales_order S, orders_cost C
+WHERE cust_id = 101
+AND S.id = C.order_id
+GROUP BY S.cust_id;
 
+--quastion 14: Create a view that calculate for each employee his age
+--quastion 14 A: Create the view
+CREATE VIEW employee_age
+AS SELECT emp_id AS emp_id, date('now') - birth_date AS emp_age, dept_id AS dept_id
+FROM employee;
+--quastion 14 B: Calc the avg age for each dept
+SELECT dept_id, AVG(emp_age)
+FROM employee_age
+GROUP BY dept_id;
 
+--quastion 15: Create a new table for manucaturer that will include id, manuf_name, address, balance.
+CREATE TABLE manufacturer (id integer not null,
+manuf_name char(30),
+address char(30),
+balance integer,
+primary key (id));
 
+--quastion 16: Add 3 listings to the manufacturer table
+--listing 1:
+INSERT INTO manufacturer
+VALUES (00,'Adidas','Somewhere',10000);
+--listing 1:
+INSERT INTO manufacturer
+VALUES (01,'Nike','Over',100000);
+--listing 1:
+INSERT INTO manufacturer
+VALUES (02,'Puma','The Rainbow',1000);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+SELECT * FROM manufacturer;
 
 
 
